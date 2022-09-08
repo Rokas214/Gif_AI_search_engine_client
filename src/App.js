f29bef3324a9e3356e9a8d4c3679018f57dc35b5
Rokas214 <rokas.guzys@gmail.com>
2022-09-08 20:47:44 +0300

    Connected to giphy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [search, setSearch] = useState("");
+	const [data, setData] = useState("");
+	let APIKEY = "G44eknrY9zKPY4CJrkgqk3j0h0x6MGC9";
+	return (
+		<div className='App'>
+			<form>
+				<label for='search'>Search</label>
+				<input
+					id='search'
+					type='search'
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<button
+					id='btnSearch'
+					onClick={(e) => {
+						e.preventDefault();
+						let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+						url = url.concat(search);
+						console.log(url);
+						fetch(url)
+							.then((res) => res.json())
+							.then((data) => {
+								console.log(data.data);
+								setData(data.data[0].images.downsized.url);
+							})
+							.catch((err) => console.log(err));
+					}}>
+					Go
+				</button>
+			</form>
+			{data && <img src={data} />}
+			{/* {data &&
+				data.map((item) => {
+					<img src={item} />;
+				})} */}
+		</div>
+	);
 }
 
 export default App;
