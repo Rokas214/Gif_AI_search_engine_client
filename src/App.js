c340a9fd851764e3b5a314a8a51d3a6d5690197a
Rokas214 <rokas.guzys@gmail.com>
2022-09-09 15:13:03 +0300

    Login created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Routes>
 				<Route exact path='/' element={<Home />} />
 				<Route exact path='/login' element={<Login />} />
+				<Route exact path='/register' element={<Register />} />
 			</Routes>
 		</Router>
 	);
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,60 @@
 import React, { useState } from "react";
 import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
 const Login = () => {
+	const [inputs, setInputs] = useState();
+	const [getNotification, setNotification] = useState(false);
+
+	let navigate = useNavigate();
+
 	return (
 		<>
+			<div>
+				<Link to={"/home"}>Home</Link>
+			</div>
 			<div className='login-form'>
-				<form>
+				<h1>Login</h1>
+				{getNotification && (
+					<div className='login-error'>{getNotification}</div>
+				)}
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						fetch("http://localhost:8080/auth/login", {
+							method: "POST",
+							headers: {
+								"Content-Type": "Application/json",
+							},
+							body: JSON.stringify(inputs),
+						})
+							.then((res) => res.json())
+							.then((data) => {
+								if (!data.token) {
+									setNotification(data.err);
+								} else {
+									localStorage.setItem("token", data.token);
+									localStorage.setItem("email", inputs.email);
+									navigate("/");
+								}
+							});
+					}}>
 					<input
 						type='email'
 						className='email'
 						placeholder='example@email.com'
+						onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
 					/>
-					<input type='password' placeholder='Password' />
-					<Button className='btn'>Login</Button>
-					<h2>Not a user?</h2>
+					<input
+						type='password'
+						placeholder='Password'
+						onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+					/>
+					<Button>Login</Button>
+					<h2>
+						Not a user? <Link to='/register'>Register here</Link>
+					</h2>
 				</form>
 			</div>
 		</>
