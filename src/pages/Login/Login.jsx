56f099b5cff59d14eedb14c69f6014afc8ee839e
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 09:26:12 +0300

    testing

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./login.css";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Nav from "../../components/Nav/Nav";
