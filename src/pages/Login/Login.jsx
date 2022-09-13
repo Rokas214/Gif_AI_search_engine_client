186d36908fd8ea978008ffc1b13f587a42a5708e
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 09:31:43 +0300

    Testing with aws

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Nav from "../../components/Nav/Nav";
